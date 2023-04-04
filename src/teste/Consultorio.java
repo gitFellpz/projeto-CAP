47298ff91ebbaaa562a1c789afc5b35c334bfbb8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-04 09:49:22 -0300

    Concluido

--- src/teste/Consultorio.java
+++ src/teste/Consultorio.java
@@ -9,7 +9,7 @@ public class Consultorio {
 		Scanner input = new Scanner(System.in);
 		
 		FilaString filaPacientes = new FilaString();
-		int opcao = 0, rm;
+		int opcao = -1;
 		
 		//Inicializando a fila
 		filaPacientes.init();
@@ -19,6 +19,40 @@ public class Consultorio {
 						"0 - Encerrar atendimento."
 					+ "\n1 - inserir paciente na fila."
 					+ "\n2 - Atender paciente.");
+			opcao = input.nextInt();
+			
+			switch(opcao) {
+				case 0:
+					//Encerra se a fila estiver vazia
+					if(filaPacientes.isEmpty())
+						System.out.println("Encerrando o atendimento!");
+					else {
+						System.out.println("Ainda ha pacientes aguardando atendimento");
+						opcao = -1;
+					}
+					break;
+					
+				case 1:
+					//insere nome do paciente que entra na fila
+					input.nextLine();
+					//O enter causa erro na aplicação.
+					
+					System.out.print("\nNome do paciente: ");
+					String nome = input.next();
+					filaPacientes.enqueue(nome);
+					break;
+					
+				case 2:
+					//Retira o proximo paciente a ser atendido da fila
+					if(filaPacientes.isEmpty())
+						System.out.println("Não ha pacientes na fila.");
+					else
+						System.out.println("Proximo paciente a ser atendido: " + filaPacientes.dequeue());
+					break;
+					
+				default:
+					System.out.println("Opção inválida.");
+			}
 		}
 		
 		input.close();
